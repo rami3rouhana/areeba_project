895188de99525dd6a32c11496c25b385240a245e
Ramy Rouhana <80242717+rami3rouhana@users.noreply.github.com>
2023-07-31 18:42:07 +0300

    refactor server url

--- frontend/src/CustomerComponent.js
+++ frontend/src/CustomerComponent.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', 
+    baseURL: 'http://localhost/web_service', 
   });
   
 const CustomerTable = () => {
