6b8f60765a68879c6301b6c6c225418d236999d8
Ramy Rouhana <80242717+rami3rouhana@users.noreply.github.com>
2023-07-31 14:11:45 +0300

    configure project

--- web_service/app.mjs
+++ web_service/app.mjs
@@ -1,5 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    credentials: true,
+    optionSuccessStatus: 200
+  }
+  
+  app.use(cors(corsOptions));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Accept-Encoding','gzip, deflate, br');
+    next();
+  });
 
 export default app;
